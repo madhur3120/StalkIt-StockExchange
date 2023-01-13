0bb779d84b28050b343095e7b05d4b09a92f692a
sanju <sanjusabu066@gmail.com>
2023-01-13 23:48:57 +0530

    data entred to mongo

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+// const cipla = require("./models/table");
 // const HttpError = require("./models/http-error");
 // const userRoutes = require("./routes/user");
 // const portRoutes = require("./routes/portfolio");
