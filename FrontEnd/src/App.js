cce0502fa9e9399c5bc14e7b7b89193c757f68af
Meet Jain <93723420+MeetJain20@users.noreply.github.com>
2023-01-15 02:05:51 +0530

    app.js changes

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import Login from "./components/Login/Login";
+import { useCallback, useId, useState, useEffect } from "react";
+import { AuthContext } from "./context/authcontext";
 import SignUp from "./components/SignUp/SignUp";
 import CompDropdown from "./components/dropdown/companies";
 import MarketDropdown from "./components/dropdown/nsedropdown";
@@ -13,16 +15,69 @@ import {
 import Home from "./components/Home/Home";
 import Dropdown from "./components/dropdown/companies";
 function App() {
-  return (
-    <Router>
-      <Routes>
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // const [LoggedIn, setLoggedIn] = useState(false);
+  const [userId, setuserId] = useState("");
+  // const [userNumber, setuserNumber] = useState(0);
+
+  const login = useCallback((uid) => {
+    localStorage.setItem("userid", uid);
+    setIsLoggedIn(true);
+    setuserId(localStorage.getItem("userid"));
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+    setuserId(null);
+  }, []);
+
+  useEffect(() => {
+    if (localStorage.hasOwnProperty("userid")) {
+      setuserId(localStorage.getItem("userid"));
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  let routes;
+  if (isLoggedIn) {
+    routes = (
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<SignUp />} />
+          <Route path="/drop" element={<CompDropdown />} />
+          <Route path="/market" element={<MarketDropdown />} />
+        </Routes>
+      </Router>
+    );
+  } else {
+    routes = (
+      <Router>
+        <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<SignUp />} />
         <Route path="/drop" element={<CompDropdown />} />
         <Route path="/market" element={<MarketDropdown />} />
-      </Routes>
-    </Router>
+        </Routes>
+      </Router>
+    );
+  }
+
+  return (
+    <>
+    <AuthContext.Provider
+        value={{
+          isLoggedIn: isLoggedIn,
+          userId: userId,
+          login: login,
+          logout: logout,
+        }}
+      >
+        <main>{routes}</main>
+      </AuthContext.Provider>
+    </>
   );
 }
 
